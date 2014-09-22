d3ebd538ca6e4b722b261d4e0eea9c39b02cb592
pwnjack <pwnjack@gmail.com>
2014-09-22 19:03:24 +0200

    Experimental

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Other dependencies: git CLI (for the first setup)
 
 ## What it does
 
-Once you started the script, all files will be copied from the /src folder to the /public folder and every file type will be treated differently before outputting in the /public folder.
+Once you started the script, all files will be copied from the /app folder to the /dist folder and every file type will be treated differently before outputting in the /dist folder.
 
 - All the main files of the defined Bower packages will be copied into their respective destination folders (css, js, fonts).
 - Less files will be compiled in plain CSS and automagically vendor-prefixed for old browsers
@@ -60,7 +60,7 @@ Now build everything up
     gulp build
 
 
-Check if the /public folder has been created succesfully, if it's there, everything went fine, you are done. Every file should be there in their respective folder (js/css/font). The script will build and output both, a minimized file (suffixed with .min) and an un-minimized development version too.
+Check if the /dist folder has been created succesfully, if it's there, everything went fine, you are done. Every file should be there in their respective folder (js/css/font). The script will build and output both, a minimized file (suffixed with .min) and an un-minimized development version too.
 
 To start the watch task that will real-time update your compiled project at every file save just run:
 
@@ -75,7 +75,7 @@ There are some useful commands for specific cases like:
 
     gulp clean
 
-Clears completely the compiled folder (/public)
+Clears completely the compiled folder (/dist)
 
 
     gulp
@@ -85,9 +85,9 @@ For lazy people, after the installation part, run directly this command to build
 
     gulp libs
     
-Copy just the Bower libraries to /public folder.
+Copy just the Bower libraries to /dist folder.
 
 
 ## Workflow
 
-Always work on the /src folder, the script will automatically update the compiled counterpart (/pubic) at every file save. This way you can safely run 'gulp clean' and don't be worried of loosing anything, because the /src folder will remain untouched, and you can always compile it again.
+Always work on the /app folder, the script will automatically update the compiled counterpart (/pubic) at every file save. This way you can safely run 'gulp clean' and don't be worried of loosing anything, because the /app folder will remain untouched, and you can always compile it again.
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
 Gulpfile intended to start a new project from scratch
 The author is @pwnjack
@@ -9,133 +11,148 @@ See the README for detailed informations
 // define gulp
 var gulp = require('gulp');
 
-// define plug-ins
-var less = require('gulp-less');
-var autoprefixer = require('gulp-autoprefixer');
-var flatten = require('gulp-flatten');
-var gulpFilter = require('gulp-filter');
-var uglify = require('gulp-uglify');
-var minifycss = require('gulp-minify-css');
-var notify = require('gulp-notify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var imagemin = require('gulp-imagemin');
-var newer = require('gulp-newer');
-var clean = require('gulp-clean');
-var w3cjs = require('gulp-w3cjs');
-var mainBowerFiles = require('main-bower-files');
+// load plugins
+var $ = require('gulp-load-plugins')();
 
 // Define paths variables
-var src_path = 'src';
-var dest_path =  'public';
+var srcPath = 'app';
+var destPath =  'dist';
 
-// Copy files from /src, and push them inside /public if they are .html files, validate them before output.
-gulp.task('files', function() {
-	var htmlFilter = gulpFilter('*.html'); // define html filter
-	return gulp.src(src_path + '/*.*') // grab all files
-	.pipe(newer(dest_path)) // if no changes skip
-	.pipe(htmlFilter) // filter html files
-	.pipe(w3cjs()) // validate html files
-	.pipe(htmlFilter.restore()) // restore html filter
-	.pipe(newer(dest_path)) // if no changes skip
-	.pipe(gulp.dest(dest_path)) // output in destination folder
-	.pipe(notify("<%= file.relative %> pushed")) // notify operation
+gulp.task('styles', function () {
+    return gulp.src('app/styles/style.less')
+    	.pipe($.sourcemaps.init())
+        .pipe($.less())
+        .pipe($.autoprefixer('last 15 version', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('.tmp/styles'))
+        .pipe($.size());
 });
 
-// Compile less files in css, autoprefix them and notify when done. Ignore responsive.less because it's imported in style.less
-gulp.task('styles', function() {
-	return gulp.src([src_path + '/less/*.less', '!' + src_path + '/less/responsive.less'])
-	.pipe(less())
-	.pipe(autoprefixer('last 15 version', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	.pipe(gulp.dest(dest_path + '/css'))
-	.pipe(notify("<%= file.relative %> compiled"));
-});
-
-// Compile scripts and push in a single minimized file inside /public/js
+// Compile scripts and push in a single minimized file inside /dist/js
 gulp.task('scripts', function() {
-	return gulp.src([src_path + '/scripts/plugins.js', src_path + '/scripts/**/*.js'])
-	.pipe(concat('main.js'))
-	.pipe(gulp.dest(dest_path + '/js'))
-	.pipe(uglify())
-	.pipe(rename({
-        suffix: ".min"
-    }))
-	.pipe(gulp.dest(dest_path + '/js'))
-	.pipe(notify("Scripts task complete"))
+	return gulp.src('app/scripts/**/*.js')
+	.pipe($.jshint())
+    .pipe($.jshint.reporter(require('jshint-stylish')))
+    .pipe($.size());
+});
+
+// Build for production
+gulp.task('html', ['styles', 'scripts'], function () {
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+    var assets = $.useref.assets({searchPath: '{.tmp,app}'});
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe($.csso())
+        .pipe(cssFilter.restore())
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
 });
 
 // Images optimization, if cached skip
 gulp.task('images', function() {
-  	return gulp.src(src_path + '/images/**/*')
-  	.pipe(newer('public/img'))
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest(dest_path + '/img'))
-    .pipe(notify("Images task complete"))
+  	return gulp.src('app/images/**/*')
+  	.pipe($.newer('dist/img'))
+    .pipe($.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest(destPath + '/img'))
+    .pipe($.notify("Images task complete"))
 });
 
-// grab libraries files from bower_components, minify and push in /public
-gulp.task('libs', function() {
+// grab libraries files from bower_components, minify and push in /dist
+gulp.task('fonts', function() {
+    var fontFilter = $.filter(['*.eot', '*.woff', '*.svg', '*.ttf']);;
+    var mainBowerFiles = require('main-bower-files');
 
-	var jsFilter = gulpFilter('*.js');
-    var cssFilter = gulpFilter('*.css');
-    var fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf']);
-
-	return gulp.src(mainBowerFiles())
-
-	// grab vendor js files from bower_components, minify and push in /public
-	.pipe(jsFilter)
-	.pipe(gulp.dest(dest_path + '/js/vendor'))
-	.pipe(uglify())
-	.pipe(rename({
-        suffix: ".min"
-    }))
-	.pipe(gulp.dest(dest_path + '/js/vendor'))
-	.pipe(jsFilter.restore())
-
-	// grab vendor css files from bower_components, minify and push in /public
-	.pipe(cssFilter)
-	.pipe(gulp.dest(dest_path + '/css'))
-	.pipe(minifycss())
-	.pipe(rename({
-        suffix: ".min"
-    }))
-	.pipe(gulp.dest(dest_path + '/css'))
-	.pipe(cssFilter.restore())
-
-	// grab vendor font files from bower_components and push in /public 
+	return gulp.src(mainBowerFiles({
+	    paths: {
+	        bowerDirectory: 'app/bower_components'
+	    }
+	}))
 	.pipe(fontFilter)
-	.pipe(flatten())
-	.pipe(gulp.dest(dest_path + '/fonts'))
+	.pipe($.flatten())
+	.pipe(gulp.dest('app/fonts'))
+	.pipe($.size());
 });
 
-// watch task
-gulp.task('watch', function() {
-    // watch all / (root) files
-    gulp.watch(src_path + '/*.*', function(event) {
-      gulp.run('files');
-    });
-    // watch .less files
-    gulp.watch(src_path + '/less/**/*.less', function(event) {
-      gulp.run('styles');
-    });
-    // watch .js files
-    gulp.watch(src_path + '/scripts/**/*.js', function(event) {
-      gulp.run('scripts');
-    });
-    // watch image files
-    gulp.watch(src_path + '/images/**/*', function(event) {
-      gulp.run('images');
-    });
+gulp.task('extras', function () {
+    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
+        .pipe(gulp.dest('dist'));
 });
 
-// clean /public folder
+// clean /dist folder
 gulp.task('clean', function() {
-  	return gulp.src([dest_path], {read: false})
-    .pipe(clean());
+  	return gulp.src([destPath], {read: false})
+    .pipe($.clean());
 });
 
 // define build task that compiles everything but without starting the watch task
-gulp.task('build', ['clean', 'files', 'styles', 'scripts', 'images', 'libs']);
+gulp.task('build', ['html', 'images', 'fonts', 'extras']);
 
 // configure gulp's default task to run everything
-gulp.task('default', ['clean', 'files', 'styles', 'scripts', 'images', 'libs', 'watch']);
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+gulp.task('connect', function () {
+    var connect = require('connect');
+    var app = connect()
+        .use(require('connect-livereload')({ port: 35729 }))
+        .use(connect.static('app'))
+        .use(connect.static('.tmp'))
+        .use(connect.directory('app'));
+
+    require('http').createServer(app)
+        .listen(9000)
+        .on('listening', function () {
+            console.log('Started connect web server on http://localhost:9000');
+        });
+});
+
+gulp.task('serve', ['connect', 'styles'], function () {
+    require('opn')('http://localhost:9000');
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream;
+
+    gulp.src('app/styles/*.less')
+        .pipe(wiredep({
+            directory: 'app/bower_components'
+        }))
+        .pipe(gulp.dest('app/styles'));
+
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            exclude: ['app/bower_components/modernizr/modernizr.js' ]
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+// watch task
+gulp.task('watch', ['connect', 'serve'], function () {
+    var server = $.livereload();
+
+    // watch for changes
+    gulp.watch([
+        'app/*.html',
+        '.tmp/styles/**/*.css',
+        'app/scripts/**/*.js',
+        'app/images/**/*'
+    ]).on('change', function (file) {
+        server.changed(file.path);
+    });
+
+    gulp.watch('app/styles/**/*.less', ['styles']);
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('bower.json', ['wiredep']);
+});
